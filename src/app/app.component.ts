7fc2a06c0a39864bb8c1552783d279ab0995cdca
jpcy1998 <jpcy18@gmail.com>
2019-02-28 12:57:24 -0500

    scripts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,38 +8,38 @@ declare var $: any
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-private socket
-public mensajes:String[]=['Admin: Bienvenidos al chat 😁']
-public escr=''
-private timer
+  private socket
+  public mensajes: String[] = ['Admin: 😁']
+  public escr = ''
+  private timer
 
   constructor() {
     this.socket = io()
   }
 
   ngOnInit(): void {
-    this.socket.on('mensaje:server',(data) =>{
-      this.mensajes.push(data.usuario+': '+data.mensaje)
+    this.socket.on('mensaje:server', (data) => {
+      this.mensajes.push(data.usuario + ': ' + data.mensaje)
     })
 
-    this.socket.on('mensaje:serverEscr',(data) =>{
+    this.socket.on('mensaje:serverEscr', (data) => {
       clearTimeout(this.timer)
-      this.escr=data.usuario+' esta escribiendo . . .'
-      this.timer = setTimeout(() => this.escr='', 1000);
+      this.escr = data.usuario + ' esta escribiendo . . .'
+      this.timer = setTimeout(() => this.escr = '', 1000);
     })
   }
 
   public enviarMensaje() {
     //console.log(usuario ,mensaje)
-    this.socket.emit('mensaje:usuario',{
+    this.socket.emit('mensaje:usuario', {
       usuario: $('#usuario').val(),
       mensaje: $('#mensaje').val()
     })
     $('#mensaje').val('')
   }
 
-  public notificarEscr(){
-    this.socket.emit('mensaje:usuarioEscr',{
+  public notificarEscr() {
+    this.socket.emit('mensaje:usuarioEscr', {
       usuario: $('#usuario').val()
     })
   }
